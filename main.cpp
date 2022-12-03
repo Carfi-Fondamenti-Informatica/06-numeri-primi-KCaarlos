92de7aecbc1d94510b7aec202b3023b8623efe03
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-03 10:58:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+bool Primo(int numero);
+
+int main() {
+
+    int numero = 0;
+
+    cin >> numero;
+
+    if (Primo(numero)) {
+        cout << "numero Primo" << endl;
+    }
+    
+   else {
+        cout << "non numero  Primo" << endl;
+    }
+
 }
